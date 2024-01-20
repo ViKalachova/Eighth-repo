606bb884c1f81624bfa29aaf130ce29da64dfd1b
ViKalachova <142012394+ViKalachova@users.noreply.github.com>
2024-01-20 18:36:25 +0200

    second commit

--- factorize.py
+++ factorize.py
@@ -1,38 +1,42 @@
 import time
+import logging
 from multiprocessing import Pool, cpu_count
 
 
-def factorize(*number):
-    for n in number:
-        i = 1
-        divisors = []
-        while i ** 2 <= n:
-            if n % i == 0:
-                divisors.append(i)
-                if i != n // i:
-                    divisors.append(n // i)
-            i += 1
-        divisors.sort()
-        print(divisors)
 
-def callback(divisors):
-    print(divisors)
+def factorize(number):
+    i = 1
+    divisors = []
+    while i ** 2 <= number:
+        if number % i == 0:
+            divisors.append(i)
+            if i != number // i:
+                divisors.append(number // i)
+        i += 1
+    divisors.sort()
+    return divisors
+
+def sinhron_factorize(*numbers):
+    start = time.time()
+    result = []
+    for number in numbers:
+        result.append(factorize(number))
+    end = time.time()
+    logging.debug(f"Usual execution time: {end - start} seconds")
+    print(result)
+    return result
+
+def parallel_factorize(*numbers):
+    start = time.time()
+    with Pool(cpu_count()) as p:
+        result = p.map_async(factorize, numbers)
+    end = time.time()
+    logging.debug(f"Parallel execution time: {end - start} seconds")
+    return result
 
 
 
 if __name__ == '__main__':
-    start = time.time()
-    a = factorize(128, 255, 99999, 10651060)
-    end = time.time() - start
-    print(a, end)
-
-    start1 = time.time()
-    with Pool(cpu_count()) as p:
-        p.apply_async(factorize(128), callback=callback)
-        p.apply_async(factorize(255), callback=callback)
-        p.apply_async(factorize(99999), callback=callback)
-        p.apply_async(factorize(10651060), callback=callback)
-        p.close()
-        p.join()
-    end2 = time.time() - start1
-    print(end2)
+    logging.basicConfig(level=logging.DEBUG, format='%(levelname)s: %(message)s')
+    sinhron_factorize(128, 255, 99999, 10651060)
+    parallel_factorize(128, 255, 99999, 10651060)
